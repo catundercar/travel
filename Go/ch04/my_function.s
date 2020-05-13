fd494043a58b59682128734ea86c810b3b0fbaa6
liu_du <mrliuxiansen8023@gmail.com>
2020-05-13 16:39:24 +0800

    Go function call 01

---
+++ Go/ch04/my_function.s
@@ -0,0 +1,46 @@
+	.file	"my_function.c"
+	.text
+	.globl	my_function
+	.type	my_function, @function
+my_function:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-8(%rbp), %eax
+	movl	-4(%rbp), %edx
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	my_function, .-my_function
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$2, %esi
+	movl	$1, %edi
+	call	my_function
+	movl	%eax, -4(%rbp)
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-4)"
+	.section	.note.GNU-stack,"",@progbits
