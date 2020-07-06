beece0b74e9f150113a85aacd767e69d4b145a22
jack <mrliuxiansen8023@gmail.com>
2020-07-06 15:05:23 +0800

    mongo封装

--- Go/mongoDemo/mongoorg/engine.go
+++ Go/mongoDemo/mongoorg/engine.go
@@ -9,17 +9,115 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Engine contains a handle of mongodb and configures.
 type Engine struct {
+	Conf    *Conf
+	c       *mongo.Client
+	session *session
+}
+
+type session struct {
+	db *mongo.Database
+}
+
+// Database is an agent of mongo.Client.
+type Database struct {
+}
+
+// Conf contains options to configure a Engine instance.
+type Conf struct {
 	Host           string
 	Port           string
+	DbName         string
 	ConnectTimeout time.Duration
-
-	DbName string
-	db     *mongo.Database
+	SocketTimeout  time.Duration
+	MaxPoolSize    uint64
+	MinPoolSize    uint64
+	Direct         bool
 }
 
+// EngineOpt 可选方法
+type EngineOpt func(*Engine)
+
+// WithConf 指定配置
+func WithConf(conf *Conf) EngineOpt {
+	return func(e *Engine) {
+		e.Conf = conf
+	}
+}
+
+// DefaultTimeout 默认超时时间
+const DefaultTimeout = 10 * time.Second
+
+// Init initializes the mongodb client.
 func (e *Engine) Init() error {
-	c, err := mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s", e.Host, e.Port)))
+	opts := conf2options(e.Conf)
+	c, err := mongo.Connect(NewCtx(e.Conf.ConnectTimeout),
+		opts.ApplyURI(fmt.Sprintf("mongodb://%s:%s/%s", e.Conf.Host, e.Conf.Port, e.Conf.DbName)))
+	if err != nil {
+		return err
+	}
+
+	e.c = c
+	e.session = &session{
+		db: c.Database(e.Conf.DbName),
+	}
+	return nil
+}
+
+func conf2options(conf *Conf) *options.ClientOptions {
+	return &options.ClientOptions{
+		ConnectTimeout: &conf.ConnectTimeout,
+		SocketTimeout:  &conf.SocketTimeout,
+		MaxPoolSize:    &conf.MaxPoolSize,
+		MinPoolSize:    &conf.MinPoolSize,
+		Direct:         &conf.Direct,
+	}
+}
+
+// NewEngine returns a engine instance.
+func NewEngine(opts ...EngineOpt) *Engine {
+	e := &Engine{
+		Conf: &Conf{
+			Host:           "127.0.0.1",
+			Port:           "27017",
+			DbName:         "mongoorg",
+			ConnectTimeout: DefaultTimeout,
+			SocketTimeout:  DefaultTimeout,
+			MaxPoolSize:    10,
+			MinPoolSize:    1,
+			Direct:         true,
+		},
+	}
+
+	for _, fn := range opts {
+		fn(e)
+	}
+
+	return e
+}
+
+// DefaultCtx returns a default timeout ctx, it's 10 second.
+func DefaultCtx() context.Context {
+	return NewCtx(DefaultTimeout)
+}
+
+// NewCtx returns a ctx with a custom timeout ctx.
+func NewCtx(timeout time.Duration) context.Context {
+	ctx, _ := context.WithTimeout(context.Background(), timeout)
+
+	return ctx
+}
+
+func (e *Engine) getDB() *mongo.Database {
+	return e.session.db
+}
+
+// Insert insert a document.
+func (e *Engine) Insert(collection string, document interface{}) error {
+	db := e.getDB()
+
+	_, err := db.Collection(collection).InsertOne(NewCtx(e.Conf.SocketTimeout), document)
 	if err != nil {
 		return err
 	}
@@ -27,43 +125,28 @@ func (e *Engine) Init() error {
 	return nil
 }
 
-func (e *Engine) getDB() (*mongo.Database, error) {
-	// TODO here can set timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	if err := e.session.Client().Connect(ctx); err != nil {
-		return nil, err
-	}
-	return e.session.Client().Database(e.DbName), nil
-}
-
-func (e *Engine) InsertOne(collection string, document interface{}) error {
-	db, err := e.getDB()
-	if err != nil {
-		return err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	_, err = db.Collection(collection).InsertOne(ctx, document)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
+// InsertMany insert many documents.
 func (e *Engine) InsertMany(collection string, documents []interface{}) error {
-	db, err := e.getDB()
+	db := e.getDB()
+
+	_, err := db.Collection(collection).InsertMany(NewCtx(e.Conf.SocketTimeout), documents)
 	if err != nil {
 		return err
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
 
-	_, err = db.Collection(collection).InsertMany(ctx, documents)
-	if err != nil {
-		return err
+	return nil
+}
+
+// Upsert find one document and replace it.
+func (e *Engine) Upsert(collection string, filter interface{}, document interface{}) error {
+	db := e.getDB()
+	upsert := true
+	result := db.Collection(collection).FindOneAndReplace(NewCtx(e.Conf.SocketTimeout),
+		filter, document, &options.FindOneAndReplaceOptions{
+			Upsert: &upsert,
+		})
+	if result.Err() != nil {
+		return result.Err()
 	}
 
 	return nil
