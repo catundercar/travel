c77e54d61c4fe5453dd10dc684a61a63a58b7c29
tulip <mrliuxiansen8023@gmail.com>
2022-03-29 17:14:59 +0800

    fix: sed -i

--- linux/shell/change_proxychains_conf.sh
+++ linux/shell/change_proxychains_conf.sh
@@ -1,1 +1,1 @@
-host host.docker.internal|head -1|awk '{print $4}'|xargs -i sudo sed "s/host.docker.internal/{}/g" /etc/proxychains4.conf
+host host.docker.internal|head -1|awk '{print $4}'|xargs -i sudo sed -i "s/host.docker.internal/{}/g" /etc/proxychains4.conf
